ca137c14d4eba03e49ff528be6554bb98c9211db
alfonsomthd <alfonsomthd@protonmail.com>
2024-12-20 14:17:27 +0100

    Just invoke script
    Signed-off-by: alfonsomthd <alfonsomthd@protonmail.com>

--- tests/functional/ui/test_odf_ui_native.py
+++ tests/functional/ui/test_odf_ui_native.py
@@ -12,12 +12,18 @@ logger = logging.getLogger(__name__)
 
 def run_cmd(cmd, cwd, env):
     return subprocess.run(
-        cmd, capture_output=True, check=True, cwd=cwd, env=env, text=True
+        cmd,
+        check=True,
+        cwd=cwd,
+        env=env,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
     )
 
 
 @pytest.mark.odf_ui_native_e2e()
-def test_ui():
+def test_odf_ui():
     """
     Launch ODF console native E2E tests.
     """
@@ -26,32 +32,8 @@ def test_ui():
     env["BRIDGE_BASE_ADDRESS"] = get_ocp_url()
 
     data_dir = "./data"
-    odf_console_dir = f"{data_dir}/odf-console"
-    # Consider using quay.io/ocs-dev/odf-console-ci-runner:node20 image to launch e2e tests.
     try:
-        if not os.path.exists(odf_console_dir):
-            run_cmd(
-                [
-                    "git",
-                    "clone",
-                    "--depth=1",
-                    "git@github.com:red-hat-storage/odf-console.git",
-                ],
-                cwd=data_dir,
-                env=env,
-            )
-        if not os.path.exists(f"{odf_console_dir}/node_modules/.bin/cypress"):
-            run_cmd(["yarn", "install"], cwd=odf_console_dir, env=env)
-        run_cmd(
-            [
-                "yarn",
-                "test-cypress-headless",
-                "--spec",
-                "cypress/tests/list-page.spec.ts",
-            ],
-            cwd=odf_console_dir,
-            env=env,
-        )
+        run_cmd(["./run-native.sh"], cwd=data_dir, env=env)
     except subprocess.CalledProcessError as e:
-        logger.exception("ERROR.")
-        raise e
+        logger.error(f"\nError output:\n{e.stdout}")
+        raise
